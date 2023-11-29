87d2197deca62a7a7303dc43313ae5e919a1e003
shivam kumar <shivam.kumar@parkland.ca>
2023-11-29 18:43:57 +0530

    initial commit

---
+++ bin/cdkpipeline-demo.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import { App } from '@aws-cdk/core';
+import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack';
+
+const app = new App();
+
+new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
+  env: { account: '606277919340', region: 'us-west-2' },
+});
+
+app.synth();
+
---
+++ test/cdkpipeline-demo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkpipelineDemo from '../lib/cdkpipeline-demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdkpipeline-demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkpipelineDemo.CdkpipelineDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
